a2075b1f4b87406b9b6a3eee1e695a0f790d7d16
Artem <artmh@proton.me>
2023-09-19 22:43:49 +0300

    fix(web): fix deps

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -2,7 +2,10 @@ import { gameRouter } from "./router/game";
 import { userRouter } from "./router/user";
 import { createTRPCRouter } from "./trpc";
 
-export const appRouter = createTRPCRouter({ userRouter, gameRouter });
+export const appRouter = createTRPCRouter({
+  user: userRouter,
+  game: gameRouter,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
--- packages/api/src/router/user.ts
+++ packages/api/src/router/user.ts
@@ -1,6 +1,52 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
+import * as jwt from "jsonwebtoken";
 
 export const userRouter = createTRPCRouter({
-  createUser: publicProcedure.input(z.object({})).mutation(async () => {}),
+  createUser: publicProcedure
+    .input(
+      z.object({
+        turnstileToken: z.string(),
+        username: z.string(),
+      }),
+    )
+    .mutation(async ({ input: { username, turnstileToken }, ctx }) => {
+      await verifyTurnstileToken(turnstileToken);
+
+      const token = jwt.sign(
+        {
+          username,
+        },
+        process.env.JWT_SECRET as string,
+        {
+          expiresIn: "1d",
+        },
+      );
+
+      return {
+        token,
+      };
+    }),
 });
+
+async function verifyTurnstileToken(token: string) {
+  let formData = new FormData();
+  formData.append("secret", process.env.CLOUDFLARE_TURNSTILE_SECRET as string);
+  formData.append("response", token);
+
+  const url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
+  const result = await fetch(url, {
+    body: formData,
+    method: "POST",
+  });
+
+  const outcome = await result.json();
+  if (!outcome.success) {
+    console.error(outcome);
+
+    throw new TRPCError({
+      code: "BAD_REQUEST",
+    });
+  }
+}
