69cee1ed0bb1f75f5938def03a397ae4b6c2ad32
Artem <artmh@proton.me>
2023-09-21 00:54:29 +0300

    feat(web + api): gameplay

--- packages/api/src/movies.ts
+++ packages/api/src/movies.ts
@@ -1,4 +1,5 @@
 import { env } from "@moviemoji/env";
+import { createId } from "@paralleldrive/cuid2";
 import { kv } from "@vercel/kv";
 import OpenAI from "openai";
 import { z } from "zod";
@@ -26,20 +27,25 @@ export async function populateMovies() {
   const pipeline = kv.pipeline();
 
   for (const m of movies) {
-    const emojis = await getEmojis(m.title, m.overview);
+    try {
+      const emojis = await getEmojis(m.title, m.overview);
 
-    if (!emojis) {
+      if (!emojis) {
+        continue;
+      }
+
+      const id = createId();
+
+      pipeline
+        .hmset(`movie:${id}`, m)
+        .sadd("movie_ids", id)
+        .sadd(`movie:${id}:emojis`, ...emojis);
+    } catch (e) {
       continue;
     }
-
-    pipeline
-      .hmset(`movie:${m.id}`, m)
-      .sadd("movie_ids", m.id)
-      .sadd(`movie:${m.id}:emojis`, ...emojis);
   }
 
-  pipeline
-    .set<number>("latestTmdbPage", latestTmdbPage + 1);
+  pipeline.set<number>("latestTmdbPage", latestTmdbPage + 1);
 
   await pipeline.exec();
 }
@@ -47,12 +53,12 @@ export async function populateMovies() {
 const openai = new OpenAI({ apiKey: env.OPENAI_API_KEY });
 
 async function getEmojis(title: string, overview: string) {
-  const prompt = `Produce a list of FOUR emojis representing this movie
+  const prompt = `Produce a list of FIVE emojis representing this movie
 title: """${title}"""
 
 overview: """${overview}"""
 
-REPLY WITH A JSON ARRAY CONTAINING 4 EMOJIS REPRESENTING THE MOVIE:`;
+REPLY WITH A JSON ARRAY CONTAINING 5 EMOJIS REPRESENTING THE MOVIE:`;
 
   const completion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
@@ -74,9 +80,10 @@ async function getMovies(page: number) {
   const request = await fetch(
     "https://api.themoviedb.org/3/discover/movie?" +
       new URLSearchParams({
+        include_adult: "false",
         order_by: "vote_average.desc",
         min_vote_count: "300",
-        "primary_release_date.lte": "2020",
+        "primary_release_date.lte": "2016-01-01",
         page: page.toString(),
       }),
     {
--- packages/api/src/router/game.ts
+++ packages/api/src/router/game.ts
@@ -1,9 +1,61 @@
-import { env } from "@moviemoji/env";
-import axios from "axios";
 import { z } from "zod";
+import { kv } from "@vercel/kv";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { TRPCError } from "@trpc/server";
+import levenshtein from "fast-levenshtein";
 
 export const gameRouter = createTRPCRouter({
+  submitAnswer: protectedProcedure
+    .input(
+      z.object({
+        movieId: z.string(),
+        answer: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input: { movieId, answer } }) => {
+      const movieTitle = await kv.hget<string>(`movie:${movieId}`, "title");
+
+      if (!movieTitle) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+
+      const distance = levenshtein.get(
+        movieTitle.toLowerCase(),
+        answer.toLowerCase(),
+      );
+
+      if (
+        movieTitle.length > 6 ? distance > movieTitle.length / 3 : distance > 2
+      ) {
+        return {
+          correct: false,
+          answer: movieTitle,
+        };
+      } else {
+        return {
+          correct: true,
+        };
+      }
+    }),
+
+  getHints: protectedProcedure
+    .input(
+      z.object({
+        movieId: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input: { movieId } }) => {
+      const movieTitle = await kv.hget<string>(`movie:${movieId}`, "title");
+
+      if (!movieTitle) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+
+      return {
+        hint: movieTitle.slice(0, Math.ceil(movieTitle.length / 3)),
+      };
+    }),
+
   getItems: protectedProcedure
     .input(
       z.object({
@@ -11,44 +63,48 @@ export const gameRouter = createTRPCRouter({
       }),
     )
     .query(async ({ ctx, input: { cursor } }) => {
+      const randomMovieIds = await kv.srandmember<string[]>("movie_ids", 20);
+
+      const pipeline = kv.pipeline();
+
+      if (!randomMovieIds) {
+        throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" });
+      }
+
+      for (const id of randomMovieIds) {
+        pipeline.smembers(`movie:${id}:emojis`);
+      }
+
+      const emojiSets = await pipeline.exec<string[][]>();
+
+      const challenges: { emojis: string[]; id: string }[] = [];
+
+      for (let i = 0; i < emojiSets.length; i++) {
+        const emojiSet = emojiSets[i];
+
+        if (!emojiSet) {
+          throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" });
+        }
+
+        const movieId = randomMovieIds[i];
+
+        if (!movieId) {
+          throw new TRPCError({ code: "INTERNAL_SERVER_ERROR" });
+        }
+
+        if (emojiSet.length !== 5) {
+          continue;
+        }
+
+        challenges.push({
+          emojis: emojiSet,
+          id: movieId,
+        });
+      }
+
       return {
-        emojis: [
-          ["👋", "👋", "👋", "👋"],
-          ["🤔", "🤔", "🤔", "🤔"],
-          ["👋", "👋", "👋", "👋"],
-          ["🤔", "🤔", "🤔", "🤔"],
-          ["👋", "👋", "👋", "👋"],
-          ["🤔", "🤔", "🤔", "🤔"],
-          ["👋", "👋", "👋", "👋"],
-          ["🤔", "🤔", "🤔", "🤔"],
-          ["👋", "👋", "👋", "👋"],
-          ["🤔", "🤔", "🤔", "🤔"],
-          ["👋", "👋", "👋", "👋"],
-          ["🤔", "🤔", "🤔", "🤔"],
-        ],
+        challenges,
         nextCursor: cursor + 1,
       };
     }),
 });
-
-export const tmdb = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
-  headers: {
-    Authorization: `Bearer ${env.TMDB_API_KEY}`,
-  },
-});
-
-const movie = z
-  .object({
-    id: z.number(),
-    title: z.string(),
-    overview: z.string(),
-    poster_path: z.string(),
-  })
-  .passthrough();
-
-const movieList = z.array(movie);
-
-import { kv } from "@vercel/kv";
-import OpenAI from "openai";
-
